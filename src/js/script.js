8d0609bc709a620a76237f501d7dbf000f35dfdc
OlegYakub <abdula299@gmail.com>
2017-05-25 18:01:57 +0300

    finish main page

--- src/js/script.js
+++ src/js/script.js
@@ -1,33 +1,26 @@
 
 /*==============slick=============*/
 
-$('.autoplay').slick({
+$('.catalog__slider').slick({
   slidesToShow: 3,
   slidesToScroll: 1,
-  autoplay: true,
-  autoplaySpeed: 2000,
-  dots: true,
-  infinite: true,
-  arrows: false,
+  dots: false,
+  infinite: false,
+  arrows: true,
+  prevArrow: '.arrs__left',
+  nextArrow: '.arrs__right',
   responsive: [
     {
-      breakpoint: 1024,
-      settings: {
-        slidesToShow: 3,
-        slidesToScroll: 3,
-        infinite: true,
-        dots: true
-      }
-    },
-    {
-      breakpoint: 600,
+      breakpoint: 960,
       settings: {
         slidesToShow: 2,
-        slidesToScroll: 2
+        slidesToScroll: 1,
+
       }
     },
+    
     {
-      breakpoint: 480,
+      breakpoint: 744,
       settings: {
         slidesToShow: 1,
         slidesToScroll: 1
