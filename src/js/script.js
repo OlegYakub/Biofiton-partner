66d4fd93e000f7c45c126a126884ab34e52ec6ce
OlegYakub <abdula299@gmail.com>
2017-05-31 12:43:32 +0300

    finish html

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,7 @@
+var windowWidth = $(window).width();
+var windowHeight = $(window).height();
+
+$(document).ready(function() {
 
 /*==============slick=============*/
 
@@ -11,7 +15,7 @@ $('.catalog__slider').slick({
   nextArrow: '.arrs__right',
   responsive: [
     {
-      breakpoint: 960,
+      breakpoint: 1170,
       settings: {
         slidesToShow: 2,
         slidesToScroll: 1,
@@ -32,17 +36,67 @@ $('.catalog__slider').slick({
   ]
 });
 
-$(document).ready(function() {
+$('.burger-menu__organs').slick({
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  dots: false,
+  infinite: false,
+  arrows: true,
+ 
+});
+
+$('.burger-menu__types').slick({
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  centerMode: true,
+  dots: false,
+  infinite: false,
+  arrows: false,
+});
+
+$('.burger').click(function(){
+  $('.burger-menu').toggleClass('burger-menu--dropped');
+  $('.header__wrapper').toggleClass('header__wrapper--active');
+  $('.overlay').toggle();
+  $('.body').toggleClass('body--overflow');
+});
+
+$('#news-paginator').pagination({
+  items: 100,
+  itemsOnPage: 10,
+  cssStyle: 'light-theme',
+  displayedPages: 3,
+});
 
 
+/*================truncate==============*/
+function truncate(str, maxlength){
+  var l = str.length;
 
-/*================modal=====================*/
-	
-	$('.btn').click(function(){
-		$('.header').modal({
-		  fadeDuration: 250,
-		  fadeDelay: 0.80
-		});
-	})
-	
+  if( l > maxlength){
+    str = str.slice(0, maxlength - 3) + "...";
+    return str;
+    
+  }else{
+    return str;
+  }
+}
+
+$('.news .new__text').each(function(i, el){
+    var r = $(el).text();
+    $(el).text(truncate(r, 235));
+  });
+
+if(windowWidth < 1170 && windowWidth > 961){
+  $('.card__text').each(function(i, el){
+    var r = $(el).text();
+    $(el).text(truncate(r, 445));
+  });
+}else if( windowWidth < 960  && windowWidth > 743){
+  $('.card__text').each(function(i, el){
+    var r = $(el).text();
+    $(el).text(truncate(r, 365)); //265
+  });
+}
+
 });
